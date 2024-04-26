42629ab66c4bf757a968a49b1b9917e4d6960924
NATALIA RISUEÑO SANTIAGO <6002974@viewnext.com>
2024-04-26 13:42:58 +0200

    Primer commit, creación del proyecto

---
+++ src/main/java/com/example/eventnook/EventnookApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventnook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventnookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventnookApplication.class, args);
+	}
+
+}
