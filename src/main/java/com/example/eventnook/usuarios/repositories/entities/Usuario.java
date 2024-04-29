e64464b4799806499f3334943ee0c940917da180
vvnat <nrisuenosantiago@gmail.com>
2024-04-29 09:24:40 +0200

    Añadiendo usuarios

---
+++ src/main/java/com/example/eventnook/usuarios/repositories/entities/Usuario.java
@@ -0,0 +1,30 @@
+package com.example.eventnook.usuarios.repositories.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private String image;
+
+}
