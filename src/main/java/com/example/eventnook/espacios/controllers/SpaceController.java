51aa9b7c923560db8b1261b08d4325fe5f7ebc4d
vvnat <nrisuenosantiago@gmail.com>
2024-04-30 16:50:27 +0200

    Spaces terminado funcional :D

--- src/main/java/com/example/eventnook/espacios/controllers/SpaceController.java
+++ src/main/java/com/example/eventnook/espacios/controllers/SpaceController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,12 +29,12 @@ public class SpaceController {
     }
 
     @GetMapping("/spaces/{eventType}")
-    public ResponseEntity<List<Space>> findByEventType(Integer eventType) {
+    public ResponseEntity<List<Space>> findByEventType(@PathVariable Integer eventType) {
         return ResponseEntity.status(HttpStatus.OK).body(spaceService.findByEventType(eventType));
     }
 
     @GetMapping("/space/{id}")
-    public ResponseEntity<Space> findById(Long id) {
+    public ResponseEntity<Space> findById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(spaceService.findById(id));
     }
 }
