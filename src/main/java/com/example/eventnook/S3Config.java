3c41e2988db24b5194e6682253e0ec078d84e900
vvnat <nrisuenosantiago@gmail.com>
2024-05-15 20:53:20 +0200

    intento de configuración s3

---
+++ src/main/java/com/example/eventnook/S3Config.java
@@ -0,0 +1,35 @@
+package com.example.eventnook;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.access.key}")
+    private String awsAccessKey;
+
+    @Value("${aws.secret.key}")
+    private String awsSecretKey;
+
+    @Bean
+    public AmazonS3 s3client() {
+
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+
+        var awsS3Config = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion(Regions.EU_CENTRAL_1) // This field if not exist throws an exception
+                .setEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://0dfbda40c694bf01514dc38c041322cf.r2.cloudflarestorage.com/eventnook", "Europe"))
+                .build();
+
+        return awsS3Config;
+    }
+}
